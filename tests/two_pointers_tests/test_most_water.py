b92274831edbadbd7ae3efbf71cd55e609308458
TheLastArchive <iarchive@hotmail.com>
2023-06-07 16:18:30 +0100

    Added a few tests

---
+++ tests/two_pointers_tests/test_most_water.py
@@ -0,0 +1,20 @@
+import unittest
+#I HATE PYTHON IMPORTS I HATE PYTHON IMPORTS I HATE PYTHON IMPORTS I HATE PYTHON IMPORTS
+import os
+import sys
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+root = os.path.dirname(parent)
+sys.path.append(root)
+import Two_Pointers as tp
+
+class TestMostWater(unittest.TestCase):
+    
+    def test_maxArea(self):
+        solution = tp.Solution()
+        self.assertEqual(49, solution.maxArea([1,8,6,2,5,4,8,3,7]))
+        self.assertEqual(1, solution.maxArea([1,1]))
+
+
+if __name__ == '__main__':
+    unittest.main()
