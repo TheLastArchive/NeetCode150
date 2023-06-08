21c42a7a8662f49401dd2d938d9b50ba76587ea4
TheLastArchive <iarchive@hotmail.com>
2023-06-08 18:06:26 +0100

    Updated init for tests

--- Two_Pointers/__init__.py
+++ Two_Pointers/__init__.py
@@ -1,2 +1,5 @@
 from Two_Pointers.most_water import Solution
 from Two_Pointers.trap_water import Solution
+from Two_Pointers.three_sum import Solution 
+from Two_Pointers.two_sum_2 import Solution
+from Two_Pointers.valid_palindrome import Solution
