4b0726dcc3c5d5f53be8e26100024d827f3e8313
TheLastArchive <iarchive@hotmail.com>
2023-06-09 11:10:01 +0100

    Added tests for new solution

---
+++ tests/sliding_window_tests/test_max_profit.py
@@ -0,0 +1,22 @@
+import unittest
+#I HATE PYTHON IMPORTS I HATE PYTHON IMPORTS I HATE PYTHON IMPORTS I HATE PYTHON IMPORTS
+import os
+import sys
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+root = os.path.dirname(parent)
+sys.path.append(root)
+import Sliding_Window as sw
+
+class TestMaxProfit(unittest.TestCase):
+    def test_correct(self):
+        solution = sw.Solution()
+        self.assertEqual(5, solution.maxProfit([7,1,5,3,6,4]))
+        self.assertEqual(5, solution.maxProfit([7,3,5,1,6,4]))
+        self.assertEqual(7, solution.maxProfit([5,12,3,4]))
+        self.assertEqual(9, solution.maxProfit([3,10,4,12]))
+        self.assertEqual(0, solution.maxProfit([7,6,4,3,1]))
+
+
+if __name__ == '__main__':
+    unittest.main()
