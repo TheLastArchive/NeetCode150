6e680c0e0de2a20fe976eaea8bff3ae50420064f
TheLastArchive <iarchive@hotmail.com>
2023-06-28 13:24:30 +0100

    Removed unused init files

--- Two_Pointers/__init__.py
+++
@@ -1,5 +0,0 @@
-from Two_Pointers.most_water import Solution
-from Two_Pointers.trap_water import Solution
-from Two_Pointers.three_sum import Solution 
-from Two_Pointers.two_sum_2 import Solution
-from Two_Pointers.valid_palindrome import Solution
